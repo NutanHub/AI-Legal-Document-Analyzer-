4629e4fd5fc2d1f4b968d35f2bb91a0fadf87f72
Nutan <nutanpadrat878@gmail.com>
2026-09-24 22:03:51 +0530

    Added Spring Boot backend structure

---
+++ backend/src/test/java/AI_Legal_Document_Analyzer/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package AI_Legal_Document_Analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
